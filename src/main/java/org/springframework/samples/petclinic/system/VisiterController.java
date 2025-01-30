00a26df24b6ae7b8e8d8e6cfa6b52abf12368b28
Sukanta Dash <sukanta.dash@gmail.com>
2025-01-30 10:25:04 -0800

    Issues folder

---
+++ src/main/java/org/springframework/samples/petclinic/system/VisiterController.java
@@ -0,0 +1,31 @@
+package org.springframework.samples.petclinic.system;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class VisiterControllerer {
+
+	@GetMapping("/visitor")
+	@ResponseBody
+	public String logVisit() {
+		String logEntry = "Welcome visit: \n";
+		try {
+			Files.write(Paths.get("/app/uploads/visits.log"), logEntry.getBytes(StandardCharsets.UTF_8),
+					StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+			return "Visit logged successfully";
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			return "Error logging visit: " + e.getMessage();
+		}
+	}
+
+}
